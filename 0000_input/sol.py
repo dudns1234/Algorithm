5601607b72ce23e14dae28c949cd974b6de0f655
yujin <dudns1234@naver.com>
2023-07-27 18:36:46 +0900

    yujin풀이

--- 0000_input/sol.py
+++ 0000_input/sol.py
@@ -1,19 +1,15 @@
 import sys
 sys.stdin = open('input.txt')
 
-# N = int(input())
+TC = int(input()) #9개라는 testcase를 받고 9개를 진행
 
-# if N % 2 == 1:
-#     print('홀수')
-# else:
-#     print('짝수')
+for i in range(TC):
+    N = int(input())
 
-# N = int(input())
-
-# if N % 2 == 1:
-#     print('홀수')
-# else:
-#     print('짝수')
+    if N % 2 == 1:
+     print('홀수')
+    else:
+        print('짝수')
 
 #input.txt
 # 9 
@@ -27,50 +23,3 @@ sys.stdin = open('input.txt')
 # 8
 # 9
 # 의미 : 맨 첫번째에 있는 9는 testcase(TC) 갯수를 의미함. 그래서 첫번째는 9개의 데이터를 줄거니까 진행하라는 의미
-
-TC = int(input()) #9개라는 testcase를 받고 9개를 진행
-
-for i in range(TC):
-    N = int(input())
-
-    if N % 2 == 1:
-     print('홀수')
-    else:
-        print('짝수')
-
-# 1차원 list input 받기
-
-# numbers = input().split()
-# #print(numbers)
-# for num in numbers:
-#    int_num = int(num)
-#    if int_num % 2 == 1:
-#       print(f'{int_num}은 홀수입니다.')
-
-numbers = list(map(int,input().split()))  # map함수 : 여러개 들어가 있는 집합을 하나하나 꺼내서 int로 바꿔주는 함수
-print(numbers)
-
-for number in numbers:
-   if number % 2 == 1:
-      print(f'{number}은 홀수입니다.')
-
-# 1차원 list input 받기
-N = int(input())
-matrix = []
-
-for i in range(N):
-   numbers = list(map(int,input().split())) #글자를 숫자로
-   #print(numbers)  #1차원데이터로 출력
-   matrix.append(numbers)
-print(matrix) #2차원데이터로 출력
-
-
-for row in matrix:
-   #print(row)
-   for item in row:
-      if item == 5:
-         print('5가 있음')
-      else:
-         print('없음')
-
-
