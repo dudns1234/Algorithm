10b89f747a3570352191fdf51ff3797a3662b95d
yujin <dudns1234@naver.com>
2023-07-28 23:23:31 +0900

    aigorithm풀기

---
+++ programmers/코테_입문/문자열뒤집기.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = ''
+    for i in range(1,len(my_string)+1):
+        answer += my_string[-i]
+    return answer
+
+print(solution("jaron"))
+print(solution("bread"))
