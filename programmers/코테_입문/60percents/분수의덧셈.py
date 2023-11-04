75410f2e549d9304ded276ad94189cac0f07c2a9
yujin <dudns1234@naver.com>
2023-11-04 19:06:12 +0900

    programmers 문제 풀기 (진행중)

--- programmers/코테_입문/60percents/분수의덧셈.py
+++ programmers/코테_입문/60percents/분수의덧셈.py
@@ -1,134 +1,51 @@
 def solution(numer1, denom1, numer2, denom2):
     answer = []
 
-    if denom1 < denom2:
-        if denom2 % denom1 == 0:
-            a = denom2 // denom1
-            child = numer1 * a
-            if child % denom2 == 0:
-                answer.append(denom2//(child+numer2))
-                answer.append(1)
-            else:
-                
-                answer.append(child+numer2)
-                answer.append(denom2)
+    if denom1 == denom2:
+        answer.append(numer1 + numer2)
+        answer.append(denom1)
+        if answer[0] % answer[1] == 0:
+            answer[0] = answer[0] // answer[1]
+            answer[1] = 1
         else:
-            a = denom1 * denom2
-            b = numer1 * denom2
-            c = numer2 * denom1
-            if a % (b+c) == 0:
-                answer.append(a//(b+c))
-                answer.append(1)
+            answer_copy = answer.copy()
+            while answer_copy[1]:
+                answer_copy[0], answer_copy[1] = answer_copy[1], answer_copy[0] % answer_copy[1]
+            if answer_copy[0] > answer[0]:
+                answer[0] = answer_copy[0] // answer[0]
+                answer[1] = answer_copy[0] // answer[1]
             else:
-                answer.append(b+c)
-                answer.append(a)
-    elif denom1 > denom1:
-        if denom1 % denom2 == 0:
-            a = denom1 // denom2
-            child = numer2 * a
-            if child % denom1 == 0:
-                answer.append(denom1//(child+numer1))
-                answer.append(1)
-            else:
-                answer.append(child+numer1)
-                answer.append(denom1)
-        else:
-            a = denom1 * denom2
-            b = numer1 * denom2
-            c = numer2 * denom1
-            if a % (b+c) == 0:
-                answer.append(a//(b+c))
-                answer.append(1)
-            else:
-                answer.append(b+c)
-                answer.append(a)
+                answer[0] = answer[0] // answer_copy[0] 
+                answer[1] = answer[1] // answer_copy[0] 
+
     else:
-        a = numer1 + numer2
-        if a % denom1 == 0:
-            answer.append(a // denom1)
-            answer.append(1)
+        answer.append(numer1 * denom2 + numer2 * denom1)
+        answer.append(denom1 * denom2)
+        if answer[0] % answer[1] == 0:
+            answer[0] = answer[0] // answer[1]
+            answer[1] = 1
         else:
-            answer.append(a)
-            answer.append(denom1)
+            answer_copy = answer.copy()
+            while answer_copy[1]:
+                answer_copy[0], answer_copy[1] = answer_copy[1], answer_copy[0] % answer_copy[1]
+            if answer_copy[0] > answer[0]:
+                answer[0] = answer_copy[0] // answer[0]
+                answer[1] = answer_copy[0] // answer[1]
+            else:
+                answer[0] = answer[0] // answer_copy[0] 
+                answer[1] = answer[1] // answer_copy[0]
 
     return answer
 
 print(solution(1,2,3,4))
 print(solution(9,2,1,3))
-print(solution(999,999,999,999))
-
-    # if denom2 % denom1 == 0 or denom1 % denom2 == 0:
-    #     if denom2 > denom1:
-    #         a = denom2 // denom1
-    #         child = numer1 * a
-    #         if denom2 % child == 0:
-    #             answer.append(child // denom2)
-    #             answer.append(1)
-    #         else:
-    #             answer.append(child+numer2)
-    #             answer.append(denom2)
-    #     if denom1 > denom2 :
-    #         a = denom1 // denom2
-    #         child = numer2 * a
-    #         if denom1 % child == 0:
-    #             answer.append(child // denom1)
-    #             answer.append(1)
-    #         else:
-    #             answer.append(child+numer1)
-    #             answer.append(denom1)
-    # else:
-    #     parent = denom1 * denom2
-    #     child1 = numer1 * denom2
-    #     child2 = numer2 * denom1
-    #     answer.append(child1+child2)
-    #     answer.append(parent)
-    
-    # return answer
-    # if denom2 > denom1:
-    #     if denom2 % denom1 == 0 :
-    #         a = denom2 // denom1
-    #         child = numer1 * a
-    #         answer.append(child+numer2)
-    #         answer.append(denom2)
-    #     else:
-    #         parent : denom1 * denom2
-    #         child1 : numer1 * denom2
-    #         child2 : numer2 * denom1
-    #         answer.append(child1+child2)
-    #         answer.append(parent)
-    # else:
-    #     if denom1 % denom2 == 0 :
-    #         a = denom2 // denom1
-    #         child = numer1 * a
-    #         answer.append(child+numer2)
-    #         answer.append(denom2)
-    #     else:
-    #         parent : denom1 * denom2
-    #         child1 : numer1 * denom2
-    #         child2 : numer2 * denom1
-    #         answer.append(child1+child2)
-    #         answer.append(parent)
-    # return answer
-
-    # if denom2 % denom1 == 0 :
-    #     if denom2 > denom1:
-    #         a = denom2 // denom1
-    #         child = numer1 * a
-    #         answer.append(child+numer2)
-    #         answer.append(denom2)
-    #     if denom1 > denom2 :
-    #         a = denom1 // denom2
-    #         child = numer2 * a
-    #         answer.append(child+numer1)
-    #         answer.append(denom1)
-    # else:
-    #     parent : denom1 * denom2
-    #     child1 : numer1 * denom2
-    #     child2 : numer2 * denom1
-    #     answer.append(child1+child2)
-    #     answer.append(parent)
-        
-
-    # return answer
+print(solution(11,22,22,22))
 
 
+# 두 숫자의 최대 공약수를 구하는 함수
+# def gcd(a, b):
+#     while b:
+#         a, b = b, a % b
+#     return a
+
+# print(gcd(33,22))
